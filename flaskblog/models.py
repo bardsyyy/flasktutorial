6031069797ba50a18d9bfddcdec51de1041b08be
bards <barsdsjaverle2@gmail.com>
2025-11-28 11:44:03 +0800

    request password reset

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,7 +1,6 @@
 from datetime import datetime
-from itsdangerous import URLSafeTimedSerializer as Serializer
-from flask import current_app
-from flaskblog import db, login_manager
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flaskblog import db, login_manager, app
 from flask_login import UserMixin
 
 
@@ -19,16 +18,14 @@ class User(db.Model, UserMixin):
     posts = db.relationship('Post', backref='author', lazy=True)
 
     def get_reset_token(self, expires_sec=1800):
-        # We don't need to encode the secret key here; Serializer handles the string.
-        s = Serializer(current_app.config['SECRET_KEY'])
-        return s.dumps({'user_id': self.id})
+        s = Serializer(app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
 
     @staticmethod
     def verify_reset_token(token):
-        s = Serializer(current_app.config['SECRET_KEY'])
+        s = Serializer(app.config['SECRET_KEY'])
         try:
-            # Enforce max_age when loading the token
-            user_id = s.loads(token, max_age=1800)['user_id']
+            user_id = s.loads(token)['user_id']
         except:
             return None
         return User.query.get(user_id)
